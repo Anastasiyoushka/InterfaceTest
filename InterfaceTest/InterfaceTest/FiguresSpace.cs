5557f7f7303d7e54e89b526160a85213177f5e48
Anastasiyoushka <hardcorevsem@mail.ru>
2016-04-26 22:55:13 +0300

    my first commit

---
+++ InterfaceTest/InterfaceTest/FiguresSpace.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceTest
+{
+    interface IFigura
+    {
+        double Perimetr();
+        double Ploshad();
+    }
+
+    class Treug : IFigura
+    {
+        public double a;
+        public double b;
+        public double c;
+
+        public double p { get { return (a + b + c) / 2.0; } }
+        
+        public double Perimetr()
+        {
+            return a + b + c;
+        }
+        public double Ploshad()
+        {
+            return Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+        }
+      
+       
+    }
+    
+    class Kvadrat : IFigura
+    {
+        public double a;
+        
+        public double  Perimetr()
+        {
+            return 4 * a;
+        }
+        public double Ploshad()
+        {
+            return a * a;
+        }
+    }
+}
